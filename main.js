814bab92843dc6edcb81b81748f7de07237f4bdd
Jake Crump <dweebjar@gmail.com>
2019-01-24 15:16:58 -0600

    Update HTML with form for search field instead of just inputs. Update CSS to begin styling return data. Build function to send markup using API data. Build function to calculate Weak To for the searched for pokemon.

--- main.js
+++ main.js
@@ -1,16 +1,86 @@
-const pokeName = document.querySelector('#pokename');
 
-document.querySelector('#searchButton').addEventListener('click', () => {
-    const pokemon = document.getElementById('searchField').value;
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    document.querySelector('#pokeData').innerHTML = '';
+    const pokemon = document.querySelector('#searchField').value.toLowerCase();
     const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
     fetch(URL)
     .then(data=>{return data.json()})
-    .then(res=>{console.log(res.types[0].type.name)})
+    .then(res=>{addPoke(res.name, res.types[0].type.name, res.sprites.front_default)});
+    document.querySelector('#searchField').value = '';
 });
 
-const addName = (name) => {
+const addPoke = (name, type, img) => {
     const markup = `
     <h2>${name}</h2>
+    <img src="${img}">
+    <ul>
+        <li>Type: ${type}</li>
+        <li>Weak to: ${calcWeakness(type)}</li>
+    </ul>
     `;
-    pokeName.insertAdjacentElement('afterbegin', markup);
-}
+
+    document.querySelector('#pokeData').insertAdjacentHTML('afterbegin', markup);
+};
+
+const calcWeakness = pokeType => {
+    switch (pokeType) {
+        case 'normal': 
+            return 'Fighting';
+            break;
+        case 'fire':
+            return 'Water, Ground, Rock';
+            break;
+        case 'water':
+            return 'Electric, Grass';
+            break;
+        case 'electric':
+            return 'Ground';
+            break;
+        case 'grass':
+            return 'Fire, Ice, Poison, Flying, Bug';
+            break;
+        case 'ice':
+            return 'Fire, Fighting, Rock';
+            break;
+        case 'fighting':
+            return 'Flying, Psychic, Fairy';
+            break;
+        case 'poison':
+            return 'Ground, Psychic';
+            break;
+        case 'ground':
+            return 'Water, Grass, Ice';
+            break;
+        case 'flying':
+            return 'Electric, Ice, Rock';
+            break;
+        case 'psychic':
+            return 'Bug, Ghost, Dark';
+            break;
+        case 'bug':
+            return 'Fire, Flying, Rock';
+            break;
+        case 'rock':
+            return 'Water, Grass, Fighting, Ground';
+            break;
+        case 'ghost':
+            return 'Ghost, Dark';
+            break;
+        case 'dragon':
+            return 'Ice, Dragon, Fairy';
+            break;
+        case 'dark':
+            return 'Fighting, Bug, Fairy';
+            break;
+        case 'steel':
+            return 'Fire, Fighting, Ground';
+            break;
+        case 'fairy':
+            return 'Poison, Steel';
+            break;
+        default:
+            console.log('Type not found');
+    }
+};
